ffea2ca89fb2cc19a99dca564d987db52a0fa05c
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-10-24 19:00:43 -0600

    feature: project added

---
+++ Pomora/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pomora
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pomora/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Pomora
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
