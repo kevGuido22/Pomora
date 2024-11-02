5122de3720e5872221244b64d99300e190fbd5c9
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-11-02 15:16:48 -0600

    feat: tabBar for navigation added

--- Pomora/App.xaml.cs
+++ Pomora/App.xaml.cs
@@ -8,7 +8,7 @@ namespace Pomora
         {
             InitializeComponent();
 
-            MainPage = new PomodoroPage();
+            MainPage = new AppShell();
         }
     }
 }
--- Pomora/obj/Debug/net8.0-android/Pomora.AssemblyInfo.cs
+++ Pomora/obj/Debug/net8.0-android/Pomora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pomora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f92871b0a40bf6e94c12feb72be6bdb0e2924b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8c3cdb0a0086880e8e83d47276781761acddc9ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pomora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pomora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
