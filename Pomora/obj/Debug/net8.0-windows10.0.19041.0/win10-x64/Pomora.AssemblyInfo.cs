d33df29efb4f291875e78f5a410156ee9a2cd358
cap1112 <angelv.salazar11@gmail.com>
2024-11-02 09:33:18 -0600

    Stop button Added

--- Pomora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pomora.AssemblyInfo.cs
+++ Pomora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pomora.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pomora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+06c354640fc1856476b4790986701040d9e74860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ac9a5d728814976ba06b7faf635a05daaa7a4b23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pomora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pomora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
